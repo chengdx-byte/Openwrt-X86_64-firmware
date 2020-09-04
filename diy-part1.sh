f2594134c9be5fb53671ad33850d4ae2c7670f74
chengdx-byte <61608035+chengdx-byte@users.noreply.github.com>
2020-09-04 22:09:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git jerrykuku https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
+sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
