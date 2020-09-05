59cec945a9e21f7ad63da4e9b8e19e4e4c7fe86b
chengdx-byte <61608035+chengdx-byte@users.noreply.github.com>
2020-09-05 15:56:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,10 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git colouricon https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git colouricon https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon' feeds.conf.default
+
+
+git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git 
