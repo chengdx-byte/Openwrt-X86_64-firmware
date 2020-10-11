96e74e193475fefe15d25ca31eff5ef7158a16e3
chengdx-byte <61608035+chengdx-byte@users.noreply.github.com>
2020-10-11 14:22:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,8 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
+git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git
+git clone https://github.com/chengdx-byte/luci-app-poweroff.git
+git clone https://github.com/chengdx-byte/luci-theme-opentopd.git
+
 
